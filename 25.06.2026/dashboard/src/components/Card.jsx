ffd1e7d40560213d8e60533b818a3bcc59d39881
Devp-sriram <sriramraman100@gmail.com>
2026-06-26 14:52:12 +0530

    entitle

--- 25.06.2026/dashboard/src/components/Card.jsx
+++ 25.06.2026/dashboard/src/components/Card.jsx
@@ -1,15 +1,15 @@
+import { FaArrowRight } from "react-icons/fa";
+import {Link} from 'react-router-dom'
+
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
 function TodoCard({ item }) {
     return <Card style={{ width: '18rem' }}>
         <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <Card.Title>{item.title}</Card.Title>
+            <Card.Text>{item.count}</Card.Text>
+            <Link to={item.to}><FaArrowRight /></Link>
         </Card.Body>
     </Card>
 }
--- 25.06.2026/dashboard/src/pages/Dash.jsx
+++ 25.06.2026/dashboard/src/pages/Dash.jsx
@@ -2,11 +2,31 @@ import React from 'react'
 import Card from '../components/Card'
 
 export default function Dash() {
+    const items = [{
+        title: 'Employee',
+        count: '7',
+        to: './about'
+    },
+    {
+        title: 'Asserts',
+        count: '7',
+        to: './about'
+    },
+    {
+        title: 'Employee',
+        count: '7',
+        to: './about'
+    },
+    {
+        title: 'Employee',
+        count: '7',
+        to: './about'
+    }]
     return (
-        <div className='w-100'>
-            <h1 className='text-primary'>Dashboard</h1>
-            <div className='border rounded'>
-                <Card/>
+        <div className='w-100 m-2'>
+            <h3 className='text-primary text-start p-2'>Dashboard</h3>
+            <div className='border rounded p-2 d-flex gap-2'>
+                {items.map(item => (<Card item={item} />))}
             </div>
         </div>
     )
